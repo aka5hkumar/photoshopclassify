6f0344312747f18f6273af0d70fa0697a434e664
Akash Kumar <aka5hkumar@users.noreply.github.com>
2020-04-21 15:51:50 -0400

    Downloads properly

--- getImages.py
+++ getImages.py
@@ -7,46 +7,50 @@ Created on Tue Apr 21 09:32:28 2020
 #!/usr/bin/env python
 # coding: utf-8
 import requests
-        
+import os
 def getImages(url, search_filter, limit, posts_dict=None):
     if not posts_dict: # If no posts are passed, get post IDs and Images
         print('Getting Posts')
         resp = requests.get(url+'/'+search_filter+'/.json?count='+str(limit), headers = {'User-agent': 'photoshopbot2'})
-        if resp.status_code == 200:
+        if resp.ok:
             resp_json=resp.json()
             posts_dict={}
             for post_number in range(0, limit):
                 post_id=resp_json['data']['children'][post_number]['data']['id']# get ID
                 post_img=resp_json['data']['children'][post_number]['data']['url']# get img link
                 posts_dict[post_id]=post_img # add to Dict                
-                image_capture = requests.get(post_img, stream=True) # Save image
-                with open('./images/original/'+post_id+'.png', 'wb') as out_file:
-                    out_file.write(image_capture.content)
+                image = requests.get(post_img, allow_redirects=True, stream=True) #Request image
+                filename = ('./data/Images/'+post_id+'/p_'+post_img.rsplit("/",1)[1])
+                os.makedirs(os.path.dirname(filename), exist_ok=True)
+                open('./data/Images/'+post_id+'/p_'+post_img.rsplit("/",1)[1], 'wb').write(image.content) #save image
             getImages(url,search_filter,limit,posts_dict) # Recursive call with post dicitonary
         else:
-            print (resp.status_code)
-            return 1
+            print (resp.reason)
     else: # If posts are passed, get comment IDs and Images for each post
         print('Getting Comments')
         for post_id in posts_dict.keys():
             resp = requests.get(url+'/comments/'+post_id+'.json?depth=1', headers = {'User-agent': 'photoshopbot2'})
-            if resp.status_code == 200:
+            if resp.ok:
                 resp_json=resp.json()
                 comment_dict={}
-                for comment_number in range(0,10):
-                    comm_id=resp_json[1]['data']['children'][comment_number]['data']['id']# get ID
-                    comm_img=resp_json[1]['data']['children'][comment_number]['data']['body']# get img
-                    if comm_img.find("(")==-1: # clean Comment
-                        comm_img=comm_img[comm_img.find("(")+1:comm_img.find(")")]
-                    else:
-                        comm_img=comm_img[comm_img.find("(")+1:comm_img.find(")")]
-                    comment_dict[comm_id]=comm_img # add to Dict
-                    image_capture = requests.get(comm_img, stream=True) # Save 
-                    with open('./images/photoshopped/'+post_id+comm_id+'.png', 'wb') as out_file:
-                        out_file.write(image_capture.content)
+                for count, commment in enumerate(resp_json[1]['data']['children']):
+                    if count == 9: # count starts at 0
+                        break
+                    else:    
+                        comm_id=commment['data']['id']
+                        try:
+                            comm_img=commment['data']['body_html'].rsplit('"')[3]
+
+                        except IndexError:
+                            count -= 1
+                            pass
+                        comment_dict[comm_id]=comm_img # add to Dict
+                        image = requests.get(comm_img, allow_redirects=True, stream=True) #Request image
+                        filename = ('./data/Images/'+post_id+'/commentImage/c_'+comm_img.rsplit("/",1)[1])
+                        os.makedirs(os.path.dirname(filename), exist_ok=True)
+                        open('./data/Images/'+post_id+'/commentImage/c_'+comm_img.rsplit("/",1)[1], 'wb').write(image.content) #save image
             else:
-                print (resp.status_code)
-   
+                print (resp.reason)
 if __name__=='__main__':     
     url = 'https://www.reddit.com/r/photoshopbattles'
     limit = 10
@@ -54,3 +58,6 @@ if __name__=='__main__':
     isPost=True
     
     getImages(url,search_filter,limit)
+
+
+
