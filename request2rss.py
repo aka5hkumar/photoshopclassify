94f007fe5f27b6ad26cc087b4a42bce0b07c2bea
Akash Kumar <aka5hkumar@users.noreply.github.com>
2020-04-20 20:23:09 -0400

    pulls data and downloads photo all in one function

--- request2rss.py
+++ request2rss.py
@@ -1,23 +1,26 @@
 import requests
+
+import os.path
 # x = requests.get('https://www.reddit.com/r/photoshopbattles/top/.json?count=20', headers = {'User-agent': 'photoshopbot'})
 # print(x.status_code)
 # x = x.json()
 # print(x[1])
 
-def getPosts(url, limit, ispost):
-    top_Posts = requests.get(url+'/top/.json?count='+str(limit), headers = {'User-agent': 'photoshopbot'})
+def getPosts(url, limit):
+    top_Posts = requests.get(url+'/top/.json?count='+str(limit), headers = {'User-agent': 'photoshopbot2'})
     if top_Posts.status_code != 200:
         print (top_Posts.status_code)
     else:
         top_Posts=top_Posts.json()
         posts_dict={}
-        if ispost
         for post_number in range(0, limit):
             post_id=top_Posts['data']['children'][post_number]['data']['id']
             post_img=top_Posts['data']['children'][post_number]['data']['url']
             posts_dict[post_id]=post_img
-            download = wget.download(post_img)
-        print(posts_dict.keys())
+            #print(post_img.rsplit("/",1)[1])
+            image = requests.get(post_img, allow_redirects=True)
+            open('data/postImage/'+post_img.rsplit("/",1)[1], 'wb').write(image.content)
+    return posts_dict
 
 if __name__ == "__main__":
     getPosts('https://www.reddit.com/r/photoshopbattles',10)    
